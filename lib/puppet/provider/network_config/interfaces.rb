f1b3be3c6376cde2a59c8acb2901567c960dd4f6
Davide Ferrari <davide@trovit.com>
2016-01-28 15:42:43 +0100

    Revert "pick jessie fix from this PR https://github.com/voxpupuli/puppet-network/pull/133"
    This reverts commit 3d27c9e987232ea9887537b5a1dead05157abc03.

--- lib/puppet/provider/network_config/interfaces.rb
+++ lib/puppet/provider/network_config/interfaces.rb
@@ -11,9 +11,9 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
 
   include PuppetX::FileMapper
 
-  desc 'Debian interfaces style provider'
+  desc "Debian interfaces style provider"
 
-  confine :osfamily => :debian
+  confine    :osfamily => :debian
   defaultfor :osfamily => :debian
 
   has_feature :provider_options
@@ -29,21 +29,23 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
 
   class MalformedInterfacesError < Puppet::Error
     def initialize(msg = nil)
-      msg ||= 'Malformed debian interfaces file; cannot instantiate network_config resources'
+      msg = 'Malformed debian interfaces file; cannot instantiate network_config resources' if msg.nil?
       super
     end
   end
 
   def self.raise_malformed
     @failed = true
-    fail MalformedInterfacesError
+    raise MalformedInterfacesError
   end
 
   class Instance
+
     attr_reader :name
 
     # Booleans
     attr_accessor :onboot, :hotplug
+
 
     # These fields are going to get rearranged to resolve issue 16
     # https://github.com/adrienthebo/puppet-network/issues/16
@@ -55,7 +57,7 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
     def initialize(name)
       @name = name
 
-      @options = Hash.new { |hash, key| hash[key] = [] }
+      @options = Hash.new {|hash, key| hash[key] = []}
     end
 
     def to_hash
@@ -72,23 +74,26 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
         :options   => squeeze_options
       }
 
-      h.each_with_object({}) do |(key, val), hash|
-        hash[key] = val if val
+      h.inject({}) do |hash, (key, val)|
+        hash[key] = val unless val.nil?
+        hash
       end
     end
 
     def squeeze_options
-      @options.each_with_object({}) do |(key, value), hash|
+      @options.inject({}) do |hash, (key, value)|
         if value.size <= 1
           hash[key] = value.pop
         else
           hash[key] = value
         end
-        hash
+
+      hash
       end
     end
 
     class << self
+
       def reset!
         @interfaces = {}
       end
@@ -103,7 +108,7 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
         if all_instances[name]
           obj = all_instances[name]
         else
-          obj = new(name)
+          obj = self.new(name)
           all_instances[name] = obj
         end
 
@@ -112,33 +117,31 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
     end
   end
 
-  def self.parse_file(_filename, contents)
+  def self.parse_file(filename, contents)
     # Debian has a very irregular format for the interfaces file. The
     # parse_file method is somewhat derived from the ifup executable
     # supplied in the debian ifupdown package. The source can be found at
     # http://packages.debian.org/squeeze/ifupdown
+
 
     # The debian interfaces implementation requires global state while parsing
     # the file; namely, the stanza being parsed as well as the interface being
     # parsed.
     status = :none
     current_interface = nil
+
     lines = contents.split("\n")
-    # TODO: Join lines that end with a backslash
+    # TODO Join lines that end with a backslash
 
     # Iterate over all lines and determine what attributes they create
     lines.each do |line|
+
       # Strip off any trailing comments
       line.sub!(/#.*$/, '')
 
       case line
       when /^\s*#|^\s*$/
         # Ignore comments and blank lines
-        next
-
-      when /^source|^source-directory/
-        # ignore source|source-directory sections, it makes this provider basically useless
-        # with Debian Jessie. Please refer to man 5 interfaces
         next
 
       when /^auto|^allow-auto/
@@ -171,29 +174,45 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
         # iface <iface> <family> <method>
         # zero or more options for <iface>
 
-        raise_malformed unless line.match(/^iface\s+(\S+)\s+(\S+)\s+(\S+)/) do |matched|
-          name   = matched[1]
-          family = matched[2]
-          method = matched[3]
+        if match = line.match(/^iface\s+(\S+)\s+(\S+)\s+(\S+)/)
+          name   = match[1]
+          family = match[2]
+          method = match[3]
 
           # If an iface block for this interface has been seen, the file is
           # malformed.
-          raise_malformed if Instance[name] && Instance[name].family
+          raise_malformed if Instance[name] and Instance[name].family
 
           status = :iface
           current_interface = name
 
           # This is done automatically
-          # Instance[name].name   = name
+          #Instance[name].name   = name
           Instance[name].family = family
           Instance[name].method = method
-          Instance[name].mode   = :raw
+          # for the vlan naming conventions (a mess), see
+          # man 5 vlan-interfaces
+          case name
+            # 'vlan22'
+            when /^vlan/
+              Instance[name].mode   = :vlan
+            # 'eth2.0003' or 'br1.2'
+            when /^[a-z]{1,}[0-9]{1,}\.[0-9]{1,4}/
+              Instance[name].mode   = :vlan
+            else
+              Instance[name].mode   = :raw
+          end
+        else
+          # If we match on a string with a leading iface, but it isn't in the
+          # expected format, malformed blar blar
+          raise_malformed
         end
 
       when /^mapping/
+
         # XXX dox
+        raise Puppet::DevError, "Debian interfaces mapping parsing not implemented."
         status = :mapping
-        fail Puppet::DevError, 'Debian interfaces mapping parsing not implemented.'
 
       else
         # We're currently examining a line that is within a mapping or iface
@@ -202,92 +221,96 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
 
         case status
         when :iface
-          raise_malformed unless line.match(/(\S+)\s+(\S.*)/) do |matched|
+          if match = line.match(/(\S+)\s+(\S.*)/)
             # If we're parsing an iface stanza, then we should receive a set of
             # lines that contain two or more space delimited strings. Append
             # them as options to the iface in an array.
 
-            key = matched[1]
-            val = matched[2]
+            key = match[1]
+            val = match[2]
 
             name = current_interface
 
-            case key # rubocop:disable Metrics/BlockNesting
-            when 'address' then         Instance[name].ipaddress    = val
-            when 'netmask' then         Instance[name].netmask      = val
-            when 'mtu' then             Instance[name].mtu          = val
-            when 'vlan-raw-device' then Instance[name].mode         = :vlan
-            else Instance[name].options[key] << val
+            case key
+            when 'address';         Instance[name].ipaddress    = val
+            when 'netmask';         Instance[name].netmask      = val
+            when 'mtu';             Instance[name].mtu          = val
+            else                    Instance[name].options[key] << val
             end
+          else
+            raise_malformed
           end
         when :mapping
-          fail Puppet::DevError, 'Debian interfaces mapping parsing not implemented.'
+          raise Puppet::DevError, "Debian interfaces mapping parsing not implemented."
         when :none
           raise_malformed
         end
       end
     end
-    Instance.all_instances.map { |_name, instance| instance.to_hash }
+
+    Instance.all_instances.map {|name, instance| instance.to_hash }
   end
 
   # Generate an array of sections
-  def self.format_file(_filename, providers)
+  def self.format_file(filename, providers)
     contents = []
     contents << header
 
     # Add onboot interfaces
-    auto_interfaces = providers.select { |provider| provider.onboot == true }
-    unless auto_interfaces.empty?
+    auto_interfaces = providers.select {|provider| provider.onboot == true }
+    unless (auto_interfaces.empty?)
       stanza = []
-      stanza << 'auto ' + auto_interfaces.map(&:name).sort.join(' ')
+      stanza << "auto " + auto_interfaces.map(&:name).sort.join(" ")
       contents << stanza.join("\n")
     end
 
     # Add hotpluggable interfaces
-    hotplug_interfaces = providers.select { |provider| provider.hotplug == true }
-    unless hotplug_interfaces.empty?
+    hotplug_interfaces = providers.select {|provider| provider.hotplug == true }
+    unless (hotplug_interfaces.empty?)
       stanza = []
-      stanza << 'allow-hotplug ' + hotplug_interfaces.map(&:name).sort.join(' ')
+      stanza << "allow-hotplug " + hotplug_interfaces.map(&:name).sort.join(" ")
       contents << stanza.join("\n")
     end
 
     # Build iface stanzas
     providers.sort_by(&:name).each do |provider|
-      # TODO: add validation method
-      fail Puppet::Error, "#{provider.name} does not have a method." if provider.method.nil?
-      fail Puppet::Error, "#{provider.name} does not have a family." if provider.family.nil?
+      # TODO add validation method
+      raise Puppet::Error, "#{provider.name} does not have a method." if provider.method.nil?
+      raise Puppet::Error, "#{provider.name} does not have a family." if provider.family.nil?
 
       stanza = []
-      stanza << %(iface #{provider.name} #{provider.family} #{provider.method})
-
-      if provider.mode == :vlan
-        # if this is a :vlan mode interface than the name of the
-        # `vlan-raw-device` is implied by the `iface` name in the format
-        # fooX.<vlan>
-
-        # The valid vlan ID range is 0-4095; 4096 is out of range
-        vlan_range_regex = /[1-3]?\d{1,3}|40[0-8]\d|409[0-5]/
-        raw_device = provider.name.match(/\A([a-z]+\d+)(?::\d+|\.#{vlan_range_regex})?\Z/)[1]
-
-        stanza << %(vlan-raw-device #{raw_device})
-      end
+      stanza << %{iface #{provider.name} #{provider.family} #{provider.method}}
 
       [
         [:ipaddress, 'address'],
         [:netmask,   'netmask'],
         [:mtu,       'mtu'],
       ].each do |(property, section)|
-        stanza << "#{section} #{provider.send property}" if provider.send(property) && provider.send(property) != :absent
+        stanza << "#{section} #{provider.send property}" if provider.send(property) and provider.send(property) != :absent
       end
 
-      if provider.options && provider.options != :absent
+      if provider.mode == :vlan
+          if provider.options["vlan-raw-device"]
+            stanza << "vlan-raw-device #{provider.options["vlan-raw-device"]}"
+          else
+            vlan_range_regex = %r[\d{1,3}|40[0-9][0-5]]
+            if ! provider.name.match(%r[\A([a-z]+\d+)(?::\d+|\.#{vlan_range_regex})\Z])          
+              raise Puppet::Error, "Interface #{provider.name}: missing vlan-raw-device or wrong VLAN ID in the iface name"
+            end
+          end                 
+      end      
+
+      if provider.options and provider.options != :absent
         provider.options.each_pair do |key, val|
           if val.is_a? String
-            stanza << "    #{key} #{val}"
+            # dont print property because we've already added it to the stanza
+            if key != "vlan-raw-device"
+              stanza << "    #{key} #{val}"
+            end
           elsif val.is_a? Array
             val.each { |entry| stanza << "    #{key} #{entry}" }
           else
-            fail Puppet::Error, "#{self} options key #{key} expects a String or Array, got #{val.class}"
+            raise Puppet::Error, "#{self} options key #{key} expects a String or Array, got #{val.class}"
           end
         end
       end
@@ -295,7 +318,7 @@ Puppet::Type.type(:network_config).provide(:interfaces) do
       contents << stanza.join("\n")
     end
 
-    contents.map { |line| line + "\n\n" }.join
+    contents.map {|line| line + "\n\n"}.join
   end
 
   def self.header
